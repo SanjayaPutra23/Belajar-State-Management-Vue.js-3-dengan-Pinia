281e0a494940bbc8b9ddd9f1ee5b80fa600eda61
aim <ahmadhakim404@gmail.com>
2024-02-12 23:46:43 +0700

    know prop drilling

--- src/components/GreatGrandChild.vue
+++ src/components/GreatGrandChild.vue
@@ -1,7 +1,10 @@
-<script setup></script>
+<script setup>
+const { numbers } = defineProps(['numbers']);
+</script>
 
 <template>
 	<div>
 		<h1>Great Grand Child</h1>
+		<p>{{ numbers }}</p>
 	</div>
 </template>
